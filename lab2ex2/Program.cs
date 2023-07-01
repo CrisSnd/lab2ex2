72ae3d65e96d5c1199b4b78316c669f4659210f1
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-01 20:13:17 +0300

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va afisa factorialul unui numar n, n fiind citit de la tastatura */
+
+            int n;
+
+            Console.WriteLine("Introduceti n!:");
+            n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("\n" + factorial);
+        }
+    }
+}
